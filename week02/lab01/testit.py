ae553252eb141ae67c19a105dfeaaa4bed43f626
Young Yang <jyang@stust.edu.tw>
2023-09-12 02:43:02 +0000

    codespace

--- week02/lab01/testit.py
+++ week02/lab01/testit.py
@@ -1,27 +1,26 @@
 import unittest
 import subprocess
+import random
 
 class TestMainScript(unittest.TestCase):
     def test_main_script(self):
-        # Create a subprocess to run main.py
-        cmd = ["python3", "main.py"]
-        input_data = "5\n3\n8\n2\n"  # Simulate user input
-        expected_output = "2\n"     # Expected output
+        cmd = ["python", "main.py"]
 
-        # Run the script and capture its output
-        completed_process = subprocess.run(
-            cmd,
-            input=input_data.encode(),  # Encode the string to bytes
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            universal_newlines=True,
-        )
-
-        # Check if the script ran successfully (exit code 0)
-        self.assertEqual(completed_process.returncode, 0)
-
-        # Check if the captured output matches the expected output
-        self.assertEqual(completed_process.stdout, expected_output)
+        for _ in range(100):
+            a, b, c, d = [random.randint(1,100) for _ in range(4)]
+            input_data = f"{a}\n{b}\n{c}\n{d}\n"    
+            expected_output = f"{min(a,b,c,d)}"  # Expected output
+            print(f"Test case: {a}, {b}, {c}, {d}")
+            completed_process = subprocess.run(
+                cmd,
+                input=input_data,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,  # Use text mode for input/output
+            )
+            self.assertEqual(completed_process.returncode, 0)
+            result = completed_process.stdout.strip()
+            self.assertEqual(result, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
