679ed2ff6d42fff25dd6658b85f9bfa2485fae6f
Young Yang <jyang@stust.edu.tw>
2023-09-12 04:23:26 +0000

    codespace

--- week02/lab01/main.py
+++ week02/lab01/main.py
@@ -1,17 +1,13 @@
 num1 = int(input())
 num2 = int(input())
-num3 = int(input())
-num4 = int(input())
+
+# Insert your code here
 
 min_num = num1
 
 if num2 < min_num:
     min_num = num2
 
-if num3 < min_num:
-    min_num = num3
-
-if num4 < min_num:
-    min_num = num4
+# Insert your code here
 
 print(min_num)
